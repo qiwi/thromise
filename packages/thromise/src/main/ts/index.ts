4f7f4a0efee1b800134b57df24d879f6e4644eca
Anton Golub <mailbox@antongolub.ru>
2019-07-02 20:56:56 +0300

    feat: add Thromise and Throxy drafts

--- packages/thromise/src/main/ts/index.ts
+++ packages/thromise/src/main/ts/index.ts
@@ -1,3 +1,42 @@
-export class Thromise {}
 
-export default Thromise
+type IAnyObject = {
+  [key: string]: any
+}
+
+export class Throxy {
+
+  proxy: any
+  target: IAnyObject
+  [key: string]: any
+
+  constructor(target: IAnyObject) {
+
+    const proxy: any = new Proxy(target, {
+      get(target, name: any) {
+        if (name in target) {
+          return target[name]
+        }
+
+        Throxy.log('get', 'name=', name)
+
+        return proxy
+      },
+    })
+
+    this.target = target
+    this.proxy = proxy
+  }
+
+  static log(...args: any[]): void {
+    console.log('Throxy ', ...args)
+  }
+
+}
+
+export class Thromise<T> extends Promise<T> {
+
+  constructor(executor: (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void) {
+    super(executor)
+  }
+
+}
--- packages/thromise/src/test/ts/index.ts
+++ packages/thromise/src/test/ts/index.ts
@@ -1,7 +1,34 @@
-import Thromise from '../../main/ts'
+import {Throxy} from '../../main/ts'
 
-describe('Thromise', () => {
+describe('Throxy', () => {
   it('exposes proper default export', () => {
-    expect(Thromise).toEqual(expect.any(Function))
+    expect(Throxy).toEqual(expect.any(Function))
   })
+
+  const target = {
+    foo: 'bar',
+  }
+  const throxy = new Throxy(target)
+
+  describe('constructor', () => {
+    it('return proper instance', () => {
+      expect(throxy).toBeInstanceOf(Throxy)
+      expect(throxy.target).toBe(target)
+    })
+  })
+
+  describe('proto', () => {
+    describe('getter', () => {
+      it('returns target\'s value if exists', () => {
+        expect(throxy.proxy.foo).toBe(target.foo)
+      })
+
+      it('returns proxy otherwise', () => {
+        expect(throxy.proxy.bar).toBe(throxy.proxy)
+        expect(throxy.proxy.bar.baz).toBe(throxy.proxy)
+      })
+    })
+  })
+
+  // describe('static', () => {})
 })
