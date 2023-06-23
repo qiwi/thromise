7d6edd898cb93d182b2cc07bf2def71051aeccaa
Anton Golub <antongolub@antongolub.com>
2023-06-23 10:33:42 +0300

    chore: internal imprs

--- src/main/js/index.cjs
+++ src/main/js/index.cjs
@@ -4,37 +4,39 @@ const isPromiseLike = v => isFn(v?.then) && isFn(v?.catch)
 
 const marker = Symbol('thromise')
 
-const _loop = (cb, ctx) => {
-  const {stack, fns, resolve, reject} = ctx
-  const thromisify = fn => {
-    if (!isFn(fn)) {
-      return fn
-    }
-    if (fns.has(fn)) {
-      return fns.get(fn)
-    }
-
-    const _fn = (...args) => {
-      if (stack.length > ctx.pos) {
-        return stack[ctx.pos++]
-      }
-
-      const result = fn(...args)
-      if (isPromiseLike(result)) {
-        throw {
-          marker,
-          result,
-        }
-      }
-      stack.push(result)
-      ctx.pos++
-      return result
-    }
-    fns.set(fn, _fn)
-
-    return _fn
+const thromisify = (fn, ctx) => {
+  const {fns, stack} = ctx
+  if (!isFn(fn)) {
+    return fn
   }
-  const t = (...args) => args.length > 1 ? args.map(thromisify) : thromisify(args[0])
+  if (fns.has(fn)) {
+    return fns.get(fn)
+  }
+
+  const _fn = (...args) => {
+    if (stack.length > ctx.pos) {
+      return stack[ctx.pos++]
+    }
+
+    const result = fn(...args)
+    if (isPromiseLike(result)) {
+      throw {
+        marker,
+        result,
+      }
+    }
+    stack.push(result)
+    ctx.pos++
+    return result
+  }
+  fns.set(fn, _fn)
+
+  return _fn
+}
+
+const _loop = (cb, ctx) => {
+  const {stack, resolve, reject} = ctx
+  const t = (...args) => args.length > 1 ? args.map(fn => thromisify(fn, ctx)) : thromisify(args[0], ctx)
 
   try {
     resolve(cb(t))
@@ -55,8 +57,8 @@ const _loop = (cb, ctx) => {
   }
 }
 
-const loop = fn => new Promise((resolve, reject) =>
-  _loop(fn, {stack: [], fns: new WeakMap(), pos: 0, resolve, reject})
+const loop = cb => new Promise((resolve, reject) =>
+  _loop(cb, {stack: [], fns: new WeakMap(), pos: 0, resolve, reject})
 )
 
 module.exports = {
