90adf5ddef7e184ae1fa8aa19a778900ec0046b8
Anton Golub <antongolub@antongolub.com>
2024-01-05 11:19:19 +0300

    fix: fix typings ref, add tds tests
    closes #31

--- src/main/js/index.d.ts
+++ src/main/js/index.d.ts
@@ -2,4 +2,4 @@ type Fn = (...args: any) => any
 
 type C = (t: (fn: Fn) => Fn) => any
 
-declare function loop<R extends Fn = C>(cb: C, ...rest: Fn[]): Promise<ReturnType<R>>
+export function loop<R extends Fn = C>(cb: C, ...rest: Fn[]): Promise<ReturnType<R>>
